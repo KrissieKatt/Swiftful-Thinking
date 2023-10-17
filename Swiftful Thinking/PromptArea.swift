dd56023b054bd7ace5a58b0a2243d7512bf40378
Krissie Katt <124374091+KrissieKatt@users.noreply.github.com>
2023-10-17 11:37:35 -0400

    I created the prompt generation and prompt questions

---
+++ Swiftful Thinking/PromptArea.swift
@@ -0,0 +1,18 @@
+//
+//  Prompt Area.swift
+//  Swiftful Thinking
+//
+//  Created by Christen Nancy Knight on 10/16/23.
+//
+
+import SwiftUI
+
+struct PromptArea: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PromptArea()
+}
